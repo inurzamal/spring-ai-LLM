7556118db0a040ecbcab164381090945d5cba243
Nurzamal Islam <n.zaman98@gmail.com>
2026-01-25 18:35:55 +0530

    Google Gemini Integration- Spring Boot

---
+++ src/main/java/com/nur/SpringAiLlmApplication.java
@@ -0,0 +1,13 @@
+package com.nur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiLlmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiLlmApplication.class, args);
+	}
+
+}
