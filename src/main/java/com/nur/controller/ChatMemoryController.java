44ef3b9992cc505a4dc728b2dbb94bcc67fd1e6a
Nurzamal Islam <n.zaman98@gmail.com>
2026-03-23 00:00:41 +0530

    swagger api description

--- src/main/java/com/nur/controller/ChatMemoryController.java
+++ src/main/java/com/nur/controller/ChatMemoryController.java
@@ -1,6 +1,8 @@
 package com.nur.controller;
 
 import com.nur.services.ChatMemoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -9,11 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Chat Memory", description = "Endpoints to interact with chat memory")
 public class ChatMemoryController {
 
     private final ChatMemoryService chatMemoryService;
 
     @GetMapping("/discuss")
+    @Operation(description = "Maintains chat history using a provided conversationId.")
     public String discuss(
             @RequestParam String conversationId,
             @RequestParam String question) {
@@ -22,6 +26,7 @@ public class ChatMemoryController {
     }
 
     @GetMapping("/ask-with-memory")
+    @Operation(description = "Maintains chat history based on userId from request header.")
     public String askWithMemory(@RequestHeader("userId") String userId, @RequestParam String question) {
         return chatMemoryService.askWithMemory(userId, question);
     }
